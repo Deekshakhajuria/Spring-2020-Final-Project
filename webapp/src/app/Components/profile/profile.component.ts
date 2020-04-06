d2be1f8937d0dd61970649c1dbf8d4c5d9622ba3
Mengfan-Shi <shi.men@husky.neu.edu>
2020-04-06 19:55:13 -0400

    add profile page

--- webapp/src/app/Components/profile/profile.component.ts
+++ webapp/src/app/Components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.authService.getProfile().subscribe((profile:ProfileComponent) => {
+      this.user = profile.user;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
 
 }
+
