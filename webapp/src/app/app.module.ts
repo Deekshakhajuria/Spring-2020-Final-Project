39ebf49c9ed8c471b0cc67eae165f0a49dfebb06
Mengfan-Shi <shi.men@husky.neu.edu>
2020-04-06 19:09:58 -0400

    add components/services

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'register', component:RegisterComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'profile', component: ProfileComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
