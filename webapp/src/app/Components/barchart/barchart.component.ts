70f1d69050646ecbbe416c17dc12111c5d900b96
YuxiWang0606 <wang.yuxi1@husky.neu.edu>
2020-04-16 18:59:29 -0400

    adjust income/expense bars to appear along each other.

--- webapp/src/app/Components/barchart/barchart.component.ts
+++ webapp/src/app/Components/barchart/barchart.component.ts
@@ -43,113 +43,87 @@ export class BarchartComponent implements OnInit {
         this.Amount2[this.currentDate.getMonth()] += this.currentamount2;// push amount into bar chart with related month
       });
     })
-      this.expenseService.getAllExpense().subscribe(result => {
-        result.forEach(x => {
-          this.currentDate = new Date(x.Date);    // import Date from database)
-          this.currentamount = x.amount;          // create a currentamount array for import amount
-          this.Amount[this.currentDate.getMonth()] += this.currentamount;// push amount into bar chart with related month
-        });
-
-        this.barchart = new Chart('canvas1', {
-          type: 'bar',
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            datasets: [
-              {
-                data: this.Amount,
-                label: 'Expense',
-                borderColor: '#3cba9f',
-                backgroundColor: [ //background color code for 12 bars
-                  'rgba(255, 99, 132, 1)', //1
-                  'rgba(54, 162, 235, 1)', //2
-                  'rgba(255, 206, 86, 1)', //3
-                  'rgba(75, 192, 192, 1)', //4
-                  'rgba(153, 102, 255, 1)', //5
-                  'rgba(255, 159, 64, 1)', //6
-                  'rgba(215, 129, 233, 1)', //7
-                  'rgba(175, 211, 86, 1)', //8
-                  'rgba(35, 92, 192, 1)', //9
-                  'rgba(125, 36, 76, 1)', //10
-                  'rgba(95, 122, 192, 1)', //11
-                  'rgba(175, 122, 162, 1)', //12
-                ],
-                barPercentage: 0.1,
-                barThickness: 15,
-                maxBarThickness: 50,
-                minBarLength: 2,
-                fill: true,
-              },
-              // add Amount bar from income which will also displat on the same chart
-              {
-                data: this.Amount2,
-                label: 'Income',
-                borderColor: '#3cba9f',
-                backgroundColor: [ //background color code for 12 bars
-                  'rgba(255, 99, 132, 1)',
-                ],
-                barPercentage: 0.1,
-                barThickness: 15,
-                maxBarThickness: 50,
-                minBarLength: 2,
-                fill: true,
-              }
-            ]
-          },
-
-          options: {
-            responsive: true,
-            title: {
-              display: true,
-              text: 'Expense/Income BarChart',
-              fontSize: 20,
-            },
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [{
-                stacked: true,
-                gridLines: {
-                  offsetGridLines: true
-                },
-                ticks: {
-                  maxRotation: 0, //make sure the Month label is in the right rotation.(default is 45 degree)
-                  minRotation: 0,
-                  padding: 10,
-
-                }
-              }],
-              yAxes: [{
-                id: 'y-axis-1',
-                display: true,
-                ticks: {
-                  beginAtZero: true,
-                  callback: function (value, index, values) {
-                    return "$" + value;// use call back function to make $ currency on Y axes
-                  }
-
-                },
-
-              // }, {
-              //   id: 'y-axis-2',
-              //   display: true,
-              //   ticks: {
-              //     beginAtZero: true,
-              //     callback: function (value, index, values) {
-              //       return "$" + value;// use call back function to make $ currency on Y axes
-              //     }
-
-              //   },
-
-              // }
-              }],
-
-            }
-          }
-        });
+    this.expenseService.getAllExpense().subscribe(result => {
+      result.forEach(x => {
+        this.currentDate = new Date(x.Date);    // import Date from database)
+        this.currentamount = x.amount;          // create a currentamount array for import amount
+        this.Amount[this.currentDate.getMonth()] += this.currentamount;// push amount into bar chart with related month
       });
-    }
+      //for Expense part bar
+      var Expense_Part = {
+        data: this.Amount,
+        label: 'Expense',
+        // yAxisID: 'y-axis-1',
+        borderColor: '#3cba9f',
+        backgroundColor: [ //background color code for 12 bars
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(255, 99, 132, 1)', 
+        ],
+        barPercentage: 0.1,
+        barThickness: 20,
+        maxBarThickness: 50,
+        minBarLength: 2,
+        fill: true,
+      }
+      // for Income part bar
+      var Income_Part = {
+        data: this.Amount2,
+        label: 'Income',
+        // yAxisID: 'y-axis-2',
+        borderColor: '#3cba9f',
+        backgroundColor: [ //background color code for 12 bars
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+          'rgba(50, 205, 50, 1)',
+        ],
+        barPercentage: 0.1,
+        barThickness: 20,
+        maxBarThickness: 50,
+        minBarLength: 2,
+        fill: true,
+      }
+      // union Income and Expense together
+      var Together = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [Expense_Part, Income_Part]
+      }
+
+      // need to use this data Together otherwise those two bar will Stack together
+      this.barchart = new Chart('canvas1', {
+        type:'bar',   //chart type
+        data :Together,
+        options:{
+          responsive: true,
+          title:{
+            display: true,
+            text: 'Expense/Income Barchart',
+            fontSize:20,
+
+          }
+        }
+      });
+    });
   }
+}
 
 
 
